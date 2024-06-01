ba2092a34e21472739428f7efe64100c2154de0f
정상필의GRAM\mod45 <mod459@naver.com>
2024-06-01 11:49:07 +0900

    장바구니 페이지 테이블 수정중

--- react-semi-project/src/components/shoppingBasket/shoppingBasket.jsx
+++ react-semi-project/src/components/shoppingBasket/shoppingBasket.jsx
@@ -1,13 +1,65 @@
 import React from "react"
+import "./shoppingBasket.css"
+import { useDispatch, useSelector } from "react-redux";
+import { deleteFromCart } from "../../features/products/productslice";
 
 export default function ShoppingBasket(){
+
+    const products = useSelector((state) => state.products.products);
+
     return(
         <div className="shoppingcart-container">
             <div className="shoppingcart-inner">
                 <div className="shoppingcart-title">
-                    <p>장바구니</p>
+                    <p className="shoppingcart-title-content">장바구니</p>
                 </div>
-                <div>
+                <div className="order-table-container">
+                    <table className="order-table">
+                        <colgroup>
+                            <col style={{ width: "27px" }}/>
+                            <col style={{ width: "98px" }}/>
+                            <col style={{ width: "auto" }}/>
+                            <col style={{ width: "98px" }}/>
+                            <col style={{ width: "120px" }}/>
+                            <col style={{ width: "98px" }}/>
+                            <col style={{ width: "120px" }}/>
+                        </colgroup>
+                        
+                        <thead className="order-table-head">
+                            <tr>
+                                <th>
+                                    <input type="checkbox"></input>
+                                </th>
+                                <th>이미지</th>
+                                <th>상품정보</th>
+                                <th>수량</th>
+                                <th>구매 금액</th>
+                                <th>배송비</th>
+                                <th>삭제</th>
+                            </tr>
+                        </thead>
+
+                        <tbody className="order-table-body">
+                            <tr>
+                                <td><input type="checkbox"/></td>
+                                <td>
+                                    <img src={products[0].src} alt={products[0].productName} style={{ width: "50px", height: "auto" }} />
+                                </td>
+                                <td>
+                                    <p>{products[0].productName}</p>
+                                    <p>{products[0].color[0]}</p>
+                                </td>
+                                <td>
+                                    <input type="text" value={products[0].count} size={1}></input>
+                                </td>
+                                <td>{products[0].price}</td>
+                                <td>0원</td>
+                                <td>
+                                    {/* <input type="button" onClick={useDispatch(deleteFromCart)}/> */}
+                                </td> 
+                            </tr>
+                        </tbody>
+                    </table>
                     
                 </div>
             </div>
--- react-semi-project/src/features/products/productslice.js
+++ react-semi-project/src/features/products/productslice.js
@@ -8,7 +8,8 @@ const initialState = {
             price: 25500,
             color: ["화이트", "베이지", "라이트퍼플", "차콜", "블랙"],
             size: ["free"],
-            src: "./img/스트릿 스프레이 루즈핏 반팔티 5color.jpg"
+            src: process.env.PUBLIC_URL + "/img/스트릿 스프레이 루즈핏 반팔티 5color.jpg",
+            count : 0
         },
         {
             productName: "아이스 쿨 무지 반팔티 2color",
@@ -16,7 +17,8 @@ const initialState = {
             price: 25500,
             color: ["화이트", "블랙"],
             size: ["S/M", "L/XL", "2XL/3XL", "4XL/5XL"],
-            src: "./img/아이스 쿨 무지 반팔티 2color.jpg"
+            src: process.env.PUBLIC_URL + "/img/아이스 쿨 무지 반팔티 2color.jpg",
+            count : 0
         },
         {
             productName: "브이앤 나일론 반바지 2color",
@@ -24,7 +26,8 @@ const initialState = {
             price: 37000,
             color: ["스카이블루", "블랙"],
             size: ["S/M", "L/XL", "2XL/3XL", "4XL/5XL"],
-            src: "./img/브이앤 나일론 반바지 2color.jpg"
+            src: process.env.PUBLIC_URL + "/public/img/브이앤 나일론 반바지 2color.jpg",
+            count : 0
         }
     ]
 };
@@ -33,9 +36,22 @@ const productSlice = createSlice({
     name: 'products',
     initialState,
     reducers: {
-        // 여기에 필요한 리듀서 추가
+        increseCount(){
+            // 수량증가버튼을 누르면 count 증가
+        },
+        minusCount(){
+            // 수량감소버튼을 누르면 count 증가
+        },
+
+        addOnCart(){
+            // 장바구니 추가 버튼을 누르면 shoppingBasket으로 이동
+        },
+
+        deleteFromCart(){
+
+        }
     },
 });
 
-export const {} = productSlice.actions;
+export const {increseCount, minusCount,addOnCart, deleteFromCart} = productSlice.actions;
 export default productSlice.reducer;
