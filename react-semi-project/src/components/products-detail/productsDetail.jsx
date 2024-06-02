becdff7b6ecf92ba35cea3c9471af41d49745234
ddabbi <suminkk04@gmail.com>
2024-06-02 12:22:35 +0900

    김수민-detail수정

--- react-semi-project/src/components/products-detail/productsDetail.jsx
+++ react-semi-project/src/components/products-detail/productsDetail.jsx
@@ -1,47 +1,67 @@
+import { useState } from "react";
 import products from "../../features/products/product";
 import "./productsDetail.css";
 
 
 export default function ProductDetail(){
-    
-    
+    const [count, setCount] = useState(products[0].count);
+    const [selectedColor, setSelectedColor] = useState('');
+    const [selectedSize, setSelectedSize] = useState('');
+
+    function CountDecrease(){
+        setCount(count -1);
+    }
+    function CountIncrease(){
+        setCount(count +1);
+    }
+     
+    function GetColor(event){
+        setSelectedColor(event.target.value);
+    }
+    function GetSize(event){
+        setSelectedSize(event.target.value);
+    }
+
     return(
         <div className="detail-context-box">
             <div className="detail-img">
                 <img src={products[0].src}></img>
             </div>
             <div className="detail-info">
-                <div>{products[0].productName}</div>
-                <div>{products[0].price}</div>
+                <div className="detail-product-name">{products[0].productName}</div>
+                <div>{products[0].price}원</div>
 
-                <div>
+                <div className="detail-color-box">
                     <p>색상</p>
                         {products[0].color.map((color, i)=>(
                             <div className="detail-color-radio">
-                                <input type="radio" id={`color${i}`} name="color" value={color}/> 
+                                <input type="radio" id={`color${i}`} name="color" value={color} onChange={GetColor}/> 
                                 <label for={`color${i}`}>{color}</label>
                             </div>
                         ))}
                 </div>
-
-                <p>사이즈</p>
-                    {products[0].szie.map((size, i) =>(
-                        <div className="detail-size-radio">
-                            <input type="radio" id={`szie${i}`} name="size" value={size}></input>
-                            <label for={`szie${i}`}>{size}</label>
-                        </div>
-                    ))}
+                <div className="dtail-size-box">
+                    <p>사이즈</p>
+                        {products[0].szie.map((size, i) =>(
+                            <div className="detail-size-radio">
+                                <input type="radio" id={`szie${i}`} name="size" value={size} onChange={GetSize}></input>
+                                <label for={`szie${i}`}>{size}</label>
+                            </div>
+                        ))}
+                </div>
                 
-                
-                 <div>
-                    <button > - </button>
-                        {products[0].count}
-                    <button > + </button>
+                <div className="detail-count">
+                    
+                    <span>{selectedColor}</span>
+                    <span>{selectedSize}</span>
+                    <button onClick={CountDecrease}> - </button>
+                        {count}
+                    <button onClick={CountIncrease}> + </button>
                 </div>
 
                 <div className="total-price">
                     <p>총 결제금액</p>
-                    <p>{products[0].price * products[0].count}원</p>
+                    <p>{products[0].price * count}원</p>
                 </div>
                 <div>
                     <button className="buy-btn">바로구매</button>
